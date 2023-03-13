0d59fe1b08190ee9bf42aae69d155b67c0af8649
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-14 01:07:51 +0600

    user input updated

--- user-input.c
+++ user-input.c
@@ -5,6 +5,9 @@ int main(){
   printf("Write value for a, b and c \n");
   scanf("%d%d%d", &a,&b,&c);
   int aver= (a+b+c)/3;
+  printf("The average is=%d\n", a);
+  printf("The average is=%d\n", b);
+  printf("The average is=%d\n", c);
   printf("The average is=%d\n", aver);
 
   return 0;
