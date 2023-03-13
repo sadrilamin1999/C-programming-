66be58a68da27151b022c514dddd6a955e898e65
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-13 15:05:27 +0600

    print added

---
+++ print.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+  printf("Name:\tSadril \n");
+  printf("Country:\tBangladesh \n");
+  printf("*\n");
+  printf("**\n");
+  printf("***\n");
+  printf("Hello \nWorld!\nThis is me.");
+
+  return 0;
+}
