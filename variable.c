688de384282a01c63897c5a0644628ba64105540
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-13 16:11:33 +0600

    variable added

---
+++ variable.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+  int a,b;
+  a=10;
+  b=20;
+  printf("a=%d \nb=%d\n", a,b);
+
+  char o='S';
+  printf("%c\n", o);
+
+  float x,y,z;
+  x=5.5;
+  y=7.2;
+  z=x+y;
+  printf("%f\n", z);
+
+
+  return 0;
+}
