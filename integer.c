3d07afa73596263e1ba3eea4a781d1f65963a7d5
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-13 16:46:25 +0600

    ingerer added

---
+++ integer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+  int a=10626544;
+  long long int b=365464203423635;
+  printf("The result of a=%d\n", a);
+  printf("The resultof b=%lld\n", b);
+  return 0;
+}
