21409736d06688594b6839bcc1a6ccf01be5d9a7
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-21 02:04:28 +0600

    minimum number of array added

---
+++ minimum.c
@@ -0,0 +1,16 @@
+// Print the minimum value of array
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+  int numbers [6]={2,5,3,9,4,6};
+  int i, min= numbers[0];
+  for(i=1; i<=5; i++)
+    {
+      if(numbers[i]<min)
+      min = numbers[i];
+    }
+    printf("The minimum value is: %d", min);
+  getch();
+}
+
