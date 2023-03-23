71b2e0aee70db69a995cc39e81c96dc371d8a1eb
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-24 00:00:01 +0600

    updated

--- import-array.c
+++ import-array.c
@@ -2,17 +2,17 @@
 
 int main() {
 
-  int values[5];
+  int values[8];
 
   printf("Enter 5 integers: ");
 
-  for(int i = 0; i < 5; ++i) {
+  for(int i = 0; i < 8; ++i) {
      scanf("%d", &values[i]);
   }
 
   printf("Displaying integers: ");
 
-  for(int i = 0; i < 5; ++i) {
+  for(int i = 0; i < 8; ++i) {
      printf("%d\n", values[i]);
   }
   return 0;
