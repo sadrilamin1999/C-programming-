d3993224591205d5fd65f706f655a401076d060c
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-26 21:27:47 +0600

    even and odd sum added

---
+++ even-oddsum.c
@@ -0,0 +1,22 @@
+// Write an array & make sum of all even or odd numbers
+#include <stdio.h>
+
+int main() {
+    int numbers[6] = {2, 5, 3, 9, 4, 6};
+    int even_sum = 0;
+    int odd_sum = 0;
+    int i;
+
+    for (i = 0; i <=5; i++) {
+        if (numbers[i] % 2 == 0) {
+            even_sum += numbers[i];
+        } else {
+            odd_sum += numbers[i];
+        }
+    }
+
+    printf("Sum of even numbers: %d\n", even_sum);
+    printf("Sum of odd numbers: %d\n", odd_sum);
+
+    return 0;
+}
