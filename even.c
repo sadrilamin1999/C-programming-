0bb1113351dfcf3878069822911f3eab4ebc176e
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-26 23:54:11 +0600

    even numbers added

--- even.c
+++ even.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
 
+void main(){
+  int numbers[6] = {2, 5, 3, 9, 4, 6};
+    int i;
+
+    printf("Even numbers in the array: ");
+    for (i = 0; i <= 5; i++) {
+        if (numbers[i] % 2 == 0) {
+            printf("%d ", numbers[i]);
+        }
+    }
+
+
+  getch();
+}
