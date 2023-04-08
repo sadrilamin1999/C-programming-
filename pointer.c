d22c175eefed15858e232958515f70b2d558faae
Sadril Amin <sadrilshuvo420@gmail.com>
2023-04-09 02:10:31 +0600

    pointer updated

--- pointer.c
+++ pointer.c
@@ -4,11 +4,12 @@
 void main(){
   int x=10;
   int *y=&x;
-  printf("x= %d\n", x);
-  printf("&x= %d\n", &x);
-  printf("y= %d\n", y);
-  printf("&y= %d\n", &y);
-  printf("*y= %d\n", *y);
-
+  printf("x=%d\n", x);// value of x
+  printf("x=%d\n",&x);// address of x
+  printf("y=%d\n", *y);// y=value of x
+  printf("&y=%d", &y); //address of y
   getch();
 }
+
+
+
